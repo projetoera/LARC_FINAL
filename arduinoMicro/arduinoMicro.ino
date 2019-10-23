6ab7ba20c366c9d4b32e9703f73aac634e66bd95
M-F-Soares <52585925+M-F-Soares@users.noreply.github.com>
2019-10-22 21:33:59 -0300

    Correção do código upado erroneamente
    Erro do Maurício

---
+++ arduinoMicro/arduinoMicro.ino
@@ -0,0 +1,123 @@
+#define maxTime 10
+#define qtdMedia 20
+
+// ==================================================================================================
+// --- Definindo as funções ---
+  // BOIZÃO
+  void boizao();
+  // COR
+  void calculo_RGB();  // Função para calcular o valor de R G B de cada cor.
+  void color();        // Função para identificar a cor que o sensor identificou.
+  // SERIAL
+  void readSerial();
+// ==================================================================================================
+
+////////////////////////////////////////////////////////////////////////////////
+// BOIZÃO
+int LED = 3;
+int ledPWM = 0, tempo = 0, tempoAnt = 0;
+byte stateLed = false;
+
+void boizao(){
+  tempo = millis();
+  if((tempo - tempoAnt) >= maxTime){
+    if(stateLed){ ledPWM++;}
+    else{ ledPWM--;}
+    if(ledPWM > 255){  ledPWM = 254;  stateLed = !stateLed;}
+    else if(ledPWM < 0){  ledPWM = 1;  stateLed = !stateLed;}
+    analogWrite(LED, ledPWM);
+    tempoAnt = tempo;
+  }
+}// timer
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//COR
+int s2 = 9, s3 = 7, out = 10, GND = 4, VCC = 2;
+int mediaR, mediaG, mediaB; 
+
+void calculo_RGB(){
+  mediaR = 0;
+  mediaG = 0;
+  mediaB = 0;
+  for(int j=0; j<qtdMedia; j++){
+    //Lê a cor vermelha
+    digitalWrite(s2, LOW);   
+    digitalWrite(s3, LOW);       
+    mediaR += pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+    //Lê a cor azul
+    digitalWrite(s3, HIGH);        
+    mediaB += pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+    //Lê a cor verde
+    digitalWrite(s2, HIGH);      
+    mediaG += pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH); 
+  }
+  mediaR = mediaR/qtdMedia;
+  mediaB = mediaB/qtdMedia;
+  mediaG = mediaG/qtdMedia;
+}// calculo_RGB
+
+void color(){
+  calculo_RGB();
+//  Serial.print("Vermelho :");
+//  Serial.print(mediaR, DEC);
+//  Serial.print(" Verde : ");
+//  Serial.print(mediaG, DEC);
+//  Serial.print(" Azul : ");
+//  Serial.print(mediaB, DEC);
+//  Serial.println();
+
+  //Verifica se a cor vermelha foi detectada
+  if (mediaR < mediaB && mediaR < mediaG && mediaR < 70){
+    Serial.print("R");     
+  }
+  //Verifica se a cor verde foi detectada    
+  else if (mediaG < mediaR && mediaG < mediaB && mediaG < 130){
+    Serial.print("G");
+  }
+  //Verifica se a cor azul foi detectada    
+  else if (mediaB < mediaR && mediaB < mediaG  && mediaB < 130){
+    Serial.print("B");     
+  }
+  //Verifica se uma outra cor foi detectada
+  else{
+    Serial.print("E");
+  }
+}// color
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// SERIAL
+int eletroIma = 11; 
+
+void readSerial(){
+  if (Serial.available() > 0) {
+    char incomingByte = Serial.read();
+    switch(incomingByte){
+      case 'I': digitalWrite(eletroIma, HIGH); break;
+      case 'S': digitalWrite(eletroIma, LOW); break;
+      case 'L': color(); break;
+    }
+  }
+}// readSerial
+////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  Serial.begin(9600); 
+  // COR
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(out, INPUT);  
+  pinMode(GND, OUTPUT);
+  pinMode(VCC, OUTPUT);
+  digitalWrite(GND, LOW);  
+  digitalWrite(VCC, HIGH);
+  // BOIZÃO
+  pinMode(LED, OUTPUT);
+  pinMode(eletroIma, OUTPUT);
+}// setup
+
+void loop(){
+  boizao();
+  readSerial();
+}// loop
